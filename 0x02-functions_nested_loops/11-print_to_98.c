9be29266025d1628ef8265f4edcaa415e2865db3
myname <myemail.gmail.com>
2023-03-21 18:38:42 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,7 +10,7 @@
 void print_to_98(int n)
 {
 	int i;
-	
+
 	if (n >= 0 && n <= 98)
 	{
 		for (i = n ; i <= 98 ; i++)
@@ -33,7 +33,7 @@ void print_to_98(int n)
 	}
 	else if (n > 98)
 	{
-		for (i = n ; i >=98 ; i--)
+		for (i = n ; i >= 98 ; i--)
 		{
 			printf("%d", i);
 			if (i != 98)
