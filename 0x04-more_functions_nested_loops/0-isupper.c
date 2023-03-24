f576980f47f489a2f2a1cc7d646313ba195312ec
myname <myemail.gmail.com>
2023-03-24 07:58:31 -0700

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,13 +3,12 @@
  * _isupper - checks for uppercase character
  * @c: the character to be checked.
  *
- * Return: 1 if character is uppercase, 0 otherwise
+ * Return: 0 or 1
  */
 int _isupper(int c)
 {
-	if  (c >= 'A' && c <= 'z')
-	{
+	if (c >= 'A' && c <= 'Z')
 		return (1);
-	}
-	return (0);
+	else
+		return (0);
 }
