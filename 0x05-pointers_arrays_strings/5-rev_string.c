aae578004e1ebba554e843fbdd6f73072451fb2d
myname <myemail.gmail.com>
2023-03-27 16:35:06 -0700

    Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s: function parameter
+ * Return: 0
+ */
+void rev_string(char *s)
+{
+	int i;
+
+	for (i = 0 ; str[i] != '\0' ; i++)
+	{
+		_putchar(str[i])
+			i++;
+	}
+	_putchar('\n');
+}
