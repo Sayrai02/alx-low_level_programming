dcd1c6788c440abb9132ea7b649671bf94197263
myname <myemail.gmail.com>
2023-03-27 12:11:35 -0700

    98 Battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - takes a pointer of an int and update the value it points to
+ *@n: is the value to be replaced
+ * Return: 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
